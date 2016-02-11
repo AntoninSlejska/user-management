ea36ce75d840269e1dd73b574c6a142f8f0bea95
AntoninSlejska <antonin@slejska.eu>
2016-02-11 12:07:51 +0100

    Change the LDAP part in the login function
    The new LDAP login function enables to define more Active Directories.
    If there are more severs defined, the user will loose only one password attemt in the case of incorrect password.

--- models/forms/LoginForm.php
+++ models/forms/LoginForm.php
@@ -90,81 +90,126 @@ class LoginForm extends Model
 	{
 		//Get the user object
 		$user = $this->getUser();
+		
 		//No user found? Return incorrect username or password error 
-		if ($user == NULL) {
+		if ($user == NULL)
+		{
 		    $this->addError('password', UserManagementModule::t('front', 'Incorrect username or password.'));
 		    return false;
 		}
-		else {
-		    // If the found user is declared as an ldap user jump into this block
-		    if ($user->ldap_user) {
-		        // Get config parameters
-		        $ldap_servers = Yii::$app->user->ldapServer;
-		        $ldap_domains = Yii::$app->user->ldapDomain;
-		        // Variable to use and change later
-		        $ldap_connection_established = false;
-		        // Loop through every given ldap_server...
-		        foreach ($ldap_servers as $ldap_server) {
-		            // If the server name contains : split it and use port parameter
-		            // otherwise use the normal ldap_connect method
-		            if (strpos($ldap_server, ":") !== false) {
-		                $ldap_server_explode = explode(":", $ldap_server);
-		                $connect = ldap_connect("ldap://" . $ldap_server_explode[0], $ldap_server_explode[1]);
-		            }
-		            else {
-		                $connect = ldap_connect("ldap://" . $ldap_server);
-		            }
-		            // Set LDAP options for the connection
-		            ldap_set_option($connect, LDAP_OPT_PROTOCOL_VERSION, 3);
-		            ldap_set_option($connect, LDAP_OPT_REFERRALS, 0);
-		            // ...and also loop through every ldap_domain
-		            foreach ($ldap_domains as $ldap_domain) {
-		                // Set auth user and auth password for the connection
-		                // domain slashes were only added if length is over zero 
-		                // to prevent errors on empty domains
-		                $auth_user = '';
-		                if (strlen($ldap_domain) > 0) {
-		                    $auth_user = $ldap_domain . "\\";
-		                }
-		                $auth_user .= $this->username;
-		                $auth_pass = $this->password;
-		                // Able to connect to the server with the credentials? 
-		                // Set variable to true and break the inner foreach loop
-		                if (($bind = @ldap_bind($connect, $auth_user, $auth_pass))) {
-		                    $ldap_connection_established = true;
-		                    break;
-		                }
-		            }
-		            // There was a working ldap connection established? Close the 
-		            // connection and break the outer foreach loop
-		            if ($ldap_connection_established) {
-		                ldap_close($connect);
-		                break;
-		            }
-		        }
-		        // No connection to the LDAP server with the domain name? 
-		        // Close existing LDAP connection and return an error
-		        if (!$ldap_connection_established) {
-		            $this->addError('password', UserManagementModule::t('front', 'Incorrect username or password.'));
-		            ldap_close($connect);
-		            return false;
-		        }
-		        else {
-		            // Correct connection? Login as the given user
-		            return Yii::$app->user->login($user, $this->rememberMe ? Yii::$app->user->cookieLifetime : 0);
-		        }
-		    }
-		    else {
-	        	// No ldap user? Verify and login or return false
-			if ( $this->validate() )
+		else
+		{
+			// If the found user is declared as an ldap user jump into this block
+			if ($user->ldap_user)
 			{
-				return Yii::$app->user->login($this->getUser(), $this->rememberMe ? Yii::$app->user->cookieLifetime : 0);
+				// Get config parameters
+				$ldaps = Yii::$app->user->ldaps;
+
+				foreach ($ldaps as $ldap)
+				{
+					$ldap_servers = $ldap['ldapServers'];
+					$ldap_domains = $ldap['ldapDomains'];
+
+					// Variable to use and change later
+					$ldap_connection_established = false;
+
+					// Loop through every given ldap_server...
+					foreach ($ldap_servers as $ldap_server)
+					{
+						// If the server name contains : split it and use port parameter
+						// otherwise use the normal ldap_connect method
+						if (strpos($ldap_server, ":") !== false)
+						{
+							$ldap_server_explode = explode(":", $ldap_server);
+							if ($connect = @ldap_connect("ldap://" . $ldap_server_explode[0], $ldap_server_explode[1]))
+							{
+								$ldap_connection_established = true;
+							}
+						}
+						else
+						{
+							if ($connect = @ldap_connect("ldap://" . $ldap_server))
+							{
+								$ldap_connection_established = true;
+							}
+						}
+						// Set LDAP options for the connection
+						ldap_set_option($connect, LDAP_OPT_PROTOCOL_VERSION, 3);
+						ldap_set_option($connect, LDAP_OPT_REFERRALS, 0);
+						
+						// There was a working ldap connection established?
+						// Break the outer foreach loop
+						if ($ldap_connection_established)
+						{
+							break;
+						}
+					}
+					if ($ldap_connection_established)
+					{
+						$user_authenticated = false;
+
+						// Loop through every ldap_domain
+						foreach ($ldap_domains as $ldap_domain)
+						{
+							// Set auth user and auth password for the connection
+							// domain slashes were only added if length is over zero
+							// to prevent errors on empty domains
+							$auth_user = '';
+							
+							if (strlen($ldap_domain) > 0)
+							{
+								$auth_user = $ldap_domain . "\\";
+							}
+							
+							$auth_user .= $this->username;
+							$auth_pass = $this->password;
+							
+							// Able to connect to the server with the credentials?
+							// Set variable to true and break the inner foreach loop
+							if (($bind = @ldap_bind($connect, $auth_user, $auth_pass)))
+							{
+								$user_authenticated = true;
+								break;
+							}
+						}
+
+						if ($user_authenticated)
+						{
+							// Correct authentication? Login as the given user
+							ldap_close($connect);
+							return Yii::$app->user->login($user, $this->rememberMe ? Yii::$app->user->cookieLifetime : 0);
+						}
+						else
+						{
+							// No connection to the LDAP server with the domain name?
+							// Close existing LDAP connection and return an error
+							ldap_close($connect);
+							$this->addError('password', UserManagementModule::t('front', 'Incorrect username or password.'));
+							return false;
+						}
+					}
+					else
+					{
+						// No connection to the LDAP server with the domain name?
+						// Close existing LDAP connection and return an error
+						ldap_close($connect);
+						$this->addError('password', UserManagementModule::t('front', 'No connection to the directory service'));
+						return false;
+					}
+				}
 			}
 			else
 			{
-				return false;
+				// No ldap user? Verify and login or return false
+				if ( $this->validate() )
+				{
+					return Yii::$app->user->login($this->getUser(), $this->rememberMe ? Yii::$app->user->cookieLifetime : 0);
+				}
+				else
+				{
+					return false;
+				}
 			}
-		    }
 		}
 	}
 
